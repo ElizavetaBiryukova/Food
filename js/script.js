c8e11a767b33af3e11760948d75357d330a3dcb5
ElizavetaBiryukova <birliza@mail.ru>
2021-05-16 14:02:54 +0200

    Adds tabs

--- js/script.js
+++ js/script.js
@@ -1,35 +1,32 @@
 window.addEventListener('DOMContentLoaded', () => {
 
-    const tabs = document.querySelectorAll('.tabheader__item'),
+    let tabs = document.querySelectorAll('.tabheader__item'),
         tabsContent = document.querySelectorAll('.tabcontent'),
         tabsParent = document.querySelector('.tabheader__items');
 
-    //функция скрывает табы
-    function hideTabContent() {
+    const hideTabContent = () => {
+
         tabsContent.forEach(item => {
             item.classList.add('hide');
-            item.classList.remove('show');
+            item.classList.remove('show', 'fade');
         });
 
         tabs.forEach(item => {
             item.classList.remove('tabheader__item_active');
         });
-    }
+    };
 
-    //функция показывает табы
-    function showTabContent(i = 0) {
-        tabsContent[i].classList.add('show');
+    const showTabContent = (i = 0) => {
+        tabsContent[i].classList.add('show', 'fade');
         tabsContent[i].classList.remove('hide');
-
         tabs[i].classList.add('tabheader__item_active');
-    }
+    };
 
     hideTabContent();
     showTabContent();
 
     tabsParent.addEventListener('click', (event) => {
         const target = event.target;
-
         if (target && target.classList.contains('tabheader__item')) {
             tabs.forEach((item, i) => {
                 if (target == item) {
@@ -39,6 +36,21 @@ window.addEventListener('DOMContentLoaded', () => {
             });
         }
     });
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
