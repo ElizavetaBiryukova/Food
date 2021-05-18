440f57613583874bbda3cf1693ab4cf4d03aca90
ElizavetaBiryukova <birliza@mail.ru>
2021-05-18 22:24:42 +0200

    Adds a timer

--- js/script.js
+++ js/script.js
@@ -41,7 +41,7 @@ window.addEventListener('DOMContentLoaded', () => {
 
     // Timer
 
-    const deadline = '2020-05-17';
+    const deadline = '2021-05-19';
 
     //Функция определяет разницу между дедлайном и текущим временем
 
@@ -51,8 +51,60 @@ window.addEventListener('DOMContentLoaded', () => {
             hours = Math.floor((t / (1000 * 60 * 60) % 24)),
             minutes = Math.floor((t / 1000 / 60) % 60),
             seconds = Math.floor((t / 1000) % 60);
+
+        return {
+            'total': t,
+            'days': days,
+            'hours': hours,
+            'minutes': minutes,
+            'seconds': seconds,
+
+        };
     }
 
+    function getZero(num) {
+        if (num >= 0 && num < 10) {
+            return `0${num}`;
+        } else {
+            return num;
+        }
+    }
+
+    function setClock(selector, endtime) {
+        const timer = document.querySelector(selector),
+            days = timer.querySelector('#days'),
+            hours = timer.querySelector('#hours'),
+            minutes = timer.querySelector('#minutes'),
+            seconds = timer.querySelector('#seconds'),
+            timeInterval = setInterval(updateClock, 1000);
+
+        updateClock();
+
+        function updateClock() {
+            const t = getTimeRemaining(endtime);
+
+
+            function zeroing(timeValue, timeSelector) {
+                if (timeValue <= 0) {
+                    timeSelector.innerHTML = '00';
+                } else {
+                    timeSelector.innerHTML = getZero(timeValue);
+                }
+            }
+
+            zeroing(t.days, days);
+            zeroing(t.hours, hours);
+            zeroing(t.minutes, minutes);
+            zeroing(t.seconds, seconds);
+            
+
+            if (t.total <= 0) {
+                clearInterval(timeInterval);
+            }
+
+        }
+    }
+    setClock('.timer', deadline);
 
 
 
